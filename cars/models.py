209e25b5b13a3c43c689cb30254efb5e5eca7648
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-23 17:14:01 -0300

    feat:criacao da tabela car

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
