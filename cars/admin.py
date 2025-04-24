b665e9a2eaa63d6c034f349291b879c63cc2b336
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-24 16:56:23 -0300

    feat:criacao da tabela brand e formato de preco

--- cars/admin.py
+++ cars/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ( 'model', 'brand', 'factory_year', 'model_year', 'price')
+    list_display = ( 'model', 'brand', 'factory_year', 'model_year', 'formatted_price')
     search_fields = ('model', 'brand')
     
+    def formatted_price(self, obj):
+        if obj.price is not None:
+            return f'R$ {obj.price:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
+        return "-"
+    formatted_price.short_description = 'Price'
+
+admin.site.register(Brand, BrandAdmin)   
 admin.site.register(Car, CarAdmin)
