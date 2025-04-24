6d7cb1b1546d2987f30431edfd89a0270e1e41ea
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-24 16:00:47 -0300

    feat:configurações de padrao

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from cars.models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ( 'model', 'brand', 'factory_year', 'model_year', 'price')
+    search_fields = ('model', 'brand')
+    
+admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -7,4 +7,6 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-
+    
+    def __str__(self):
+        return self.model
