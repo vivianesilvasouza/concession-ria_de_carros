3f1ed8e3ec2adb09a48f294a03a8a5e2b9d98bdb
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-25 16:24:53 -0300

    feat:buscando carro no banco

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.all()
+    return render(request, 'cars.html',{'cars': cars}) 
