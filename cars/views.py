4802a5f24b2303ac7020a247ffc0f95f967a0521
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-28 15:26:48 -0300

    feat:filtrando com parametros de usuario

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from cars.models import Car
 
-# def cars_view(request):
-#     cars = Car.objects.all()
-#     return render(request, 'cars.html',{'cars': cars}) 
-
 def cars_view(request):
-    cars = Car.objects.filter(brand__name='Ford')
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+    
     return render(request, 'cars.html',{'cars': cars}) 
+
+# def cars_view(request):
+#     cars = Car.objects.filter(brand__name='Ford')
+#     return render(request, 'cars.html',{'cars': cars}) 
