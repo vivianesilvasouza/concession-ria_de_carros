0fefd52985c6f33a076e32745ab7f4ebe761b5dd
Viviane <vivianedasilvasouza2020@gmail.com>
2025-04-28 15:31:04 -0300

    feat:adiciona ordenacao

--- cars/views.py
+++ cars/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
     if search:
-        cars = Car.objects.filter(model__icontains=search)
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
     
     return render(request, 'cars.html',{'cars': cars}) 
 
